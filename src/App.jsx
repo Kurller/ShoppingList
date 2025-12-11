855524cdfc44574b40410880fe057b8fe758bd98
Kurller <kolaquadry@gmail.com>
2025-12-11 12:57:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ShoppingList } from "./shop.jsx";
+
+function App() {
+  return (
+    <div>
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
